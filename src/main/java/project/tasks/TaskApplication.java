fff423f166e859ab0251db585a1133a96bf194d2
Karina Ferreira <96082465+Karina2510@users.noreply.github.com>
2023-11-05 01:56:56 -0300

    excluindo comentários branch main

--- src/main/java/project/tasks/TaskApplication.java
+++ src/main/java/project/tasks/TaskApplication.java
@@ -11,8 +11,5 @@ public class TaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-
-//	public void qqq() {
-//		String id = UUID.randomUUID().toString();
-//	}
+	
 }
